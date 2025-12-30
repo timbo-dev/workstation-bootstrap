6d2013a565856e057e6d5e7948f35654235a6065
timbo-dev <dev@timbo.me>
2025-12-30 03:50:01 -0300

    refactor: use pacman_install utility function on pacman go module installation

--- setup/modules/01-go-installation/install.sh
+++ setup/modules/01-go-installation/install.sh
@@ -7,5 +7,5 @@ source "$(dirname "$0")/../../lib/utils.sh"
 echo "Installing Go for pacman installation..."
 
 # Ensure go is installed pacman package manager
-wait_for_pacman
-pacman -S --needed --noconfirm go
+pacman_install "go"
+
